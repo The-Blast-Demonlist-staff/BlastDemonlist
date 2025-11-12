46257cc2ae6afd4885e7a08de480ffe62c767f6b
Sebastianjk73 <sebastianjk70@gmail.com>
2025-11-12 08:36:01 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -25,7 +25,7 @@ export default {
                 <table class="list" v-if="list">
                     <tr v-for="([level, err], i) in list">
                         <td class="rank">
-                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-if="i + 1 <= 50" class="type-label-lg">#{{ i + 1 }}</p>
                             <p v-else class="type-label-lg">Legacy</p>
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': !level }">
@@ -100,28 +100,28 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Hacks no allowed must send the video without edit 
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        No click between frames allowed 
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        The level must be 45 seconds long 
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The level must be classic mode no plataform mode allowed
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        No mod menu's allowed
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Not use secret routes or bugs
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        The level must dont have a easy way or a hard way only unmodified way
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        If you modify the level when is on the demonlist is gonna be eliminated of the demonlist
                     </p>
                 </div>
             </div>
